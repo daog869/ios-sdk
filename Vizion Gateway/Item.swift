adfa92fe8b26698ae374985e3ae34cdf97bcb31d
daog869 <andrebrowne86@gmail.com>
2025-01-13 06:55:49 -0500

    Initial Commit

---
+++ Vizion Gateway/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vizion Gateway
+//
+//  Created by Andre Browne on 1/13/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Vizion GatewayTests/Vizion_GatewayTests.swift
@@ -0,0 +1,17 @@
+//
+//  Vizion_GatewayTests.swift
+//  Vizion GatewayTests
+//
+//  Created by Andre Browne on 1/13/25.
+//
+
+import Testing
+@testable import Vizion_Gateway
+
+struct Vizion_GatewayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
